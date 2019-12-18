cdd0999221862a0bbbfac1a4c619a6a74af05459
EthanLin <ethan510010@gmail.com>
2019-12-18 21:29:14 +0800

    測試

---
+++ deployment.sh
@@ -0,0 +1,13 @@
+WEB_PATH='/home/ec2-user/deploy'
+WEB_USER='ec2-user'
+WEB_USERGROUP='ec2-user'
+
+echo "Start Deployment"
+cd $WEB_PATH
+echo "Pulling source code..."
+
+git reset --hard origin/develop
+git clean -f
+git pull origin develop
+git checkout develop
+chown -R $WEB_USER:$WEB_USERGROUP $WEB_PATH
