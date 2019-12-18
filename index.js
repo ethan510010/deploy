e08a86076b0eb0861b2a49f0533878c0517ba95f
EthanLin <ethan510010@gmail.com>
2019-12-18 22:29:15 +0800

    修改index.js

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ http.createServer(function (req, res) {
       let sig = "sha1=" + crypto.createHmac('sha1', secret).update(chunk.toString()).digest('hex');
 
       if (req.headers['x-hub-signature'] == sig) {
+          console.log('嗨')
           exec('./deployment.sh');
       }
   });
